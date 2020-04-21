afe1b69fb331d908e76c50c634f8daab8cbe3e5a
Lucas Haroldsen <lharoldsen@mica.edu>
2020-04-21 14:28:48 -0400

    fixed

--- 1_digitalOutput_bitTwidling/blink/firmware/main.c
+++ 1_digitalOutput_bitTwidling/blink/firmware/main.c
@@ -1,28 +1,26 @@
-/* Name: main.c
- * Author: <insert your name here>
- * Copyright: <insert your copyright message here>
- * License: <insert your license reference here>
- */
+// ------- Preamble -------- //
 
-#include <avr/io.h>
-#include <util/delay.h>
+#include <avr/io.h>          //include the 
+#include <util/delay.h>      //include utility for the delay function
 
-
-//main loop 
+//-------- main loop --------//
 
 int main(void){
-//initializing
-// DDRB = 0b00001000;
-DDRB|= (1 << PB3);
 
+// -------- Inits --------- //
+// DDRB = 0b00001000;  //setting DDRB 4th pin to high (PinMode on arduino) configure it for the output
+DDRB|= (1 << PB3);     // DDRB = DDRB | (1 << PB3) shift bit to location of the pin we want and OR them to turn the PB3 pin
+
+
+// ------ Event loop ------ //
 while (1) {
 
-	PORTB = 0b00001000;
-	_delay_ms(500);
+	PORTB = 0b00001000; //output HIGH to the 4th pin of Port B
+	_delay_ms(500);     //delay 500 milli seconds
 
-	PORTB = 0b00000000;
-	_delay_ms(500);
+	PORTB = 0b00000000; //output LOW to the 4th pin of Port B
+	_delay_ms(500);      //delay 500 milli seconds
 
     }
-    return 0;   /* never reached */
+    return 0;   // never reached 
 }
