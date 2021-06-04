702d13ec0b3187f3b9d3e342892e8c2d74b8219f
Lucas Haroldsen <lharoldsen@mica.edu>
2021-06-04 14:19:57 -0400

    j

--- arduino_nano/EEPROM_study/Write_to_EEPROM_test/Write_to_EEPROM_test.ino
+++ arduino_nano/EEPROM_study/Write_to_EEPROM_test/Write_to_EEPROM_test.ino
@@ -1,57 +1,54 @@
 #include <EEPROM.h>
 
-int address = 0;
-int bottonpin = 13;
-byte value;
-int counter;
-
-
+int address = 0;  //set up variable for address of EEPROM
+int bottonpin = 13; // define pin for digital read
+byte value;        // variable to store 8 bits
+int counter;       //variable for counting age
 
 
 void setup() {
 
-  for(int x = 2; x < 6; x++){
+  for(int x = 2; x < 6; x++){     //set up digital pin 2 to 5 to output
   	pinMode(x, OUTPUT);
   }
 
-   pinMode(bottonpin, INPUT);
+   pinMode(bottonpin, INPUT);    //set up button pin to input
 
    Serial.begin(9600);
    while (!Serial) {
-   ; // wait for serial port to connect. Needed for native USB port only
+   ;                         // wait for serial port to connect
    }
-
-   counter = EEPROM.read(5);
-
 }
 
 
 
 void loop() {
 
-	for(int index = 0; index < 4; index++){
-		value = EEPROM.read(index);
-		digitalWrite(index + 2, value);
-		Serial.print(value);
+	counter = EEPROM.read(5);  // Read from address 5 and store it to variable counter
+
+	for(int index = 0; index < 4; index++){     //for loop for each addresses
+		value = EEPROM.read(index);              // read
+		digitalWrite(index + 2, value);          // and output to digital pins
+		Serial.print(value);                     // print the value to serial monitor
 	}
 
 	Serial.print("\t");
-	Serial.print(counter);
+	Serial.print(counter);                       //print age
 	Serial.println();
 
 
-	int bottonVal = digitalRead(bottonpin);
-	EEPROM.update(address, bottonVal);
+	int bottonVal = digitalRead(bottonpin);      //read from digital input
+	EEPROM.update(address, bottonVal);           //update the value for the address of this cycle
 
 
-	address = address + 1;
+	address = address + 1;                      //advance address
 
-	if (address == 4) {
+	if (address == 4) {                         //condition for reseting address
 		address = 0;
 	}
 
-	counter++;
-    EEPROM.write(5, counter);
+	counter++;                                  //advance counter
+    EEPROM.write(5, counter);                   //write to counter
 
 	
 
